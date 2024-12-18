9d5a64cb8c9271dde6ef7317ff31b463607bf37e
Jorge Luis Juárez Velázquez <jorgejuarez@charly.com.mx>
2024-12-17 18:01:37 -0600

    Ordering Domain Layer

---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record OrderName
+    {
+        private const int DEFAULT_LENGTH = 5;
+        public string Value { get; } = default!;
+        private OrderName(string value) => Value = value;
+
+        public static OrderName Of(string value)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(value);
+            ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DEFAULT_LENGTH);
+
+            return new OrderName(value);
+        }
+
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString("Database")!;
+
+        //// Add services to the container.
+        // services.AddDbContext<ApplicationDbContext>(options =>
+        //      options.UseSQlServer(connectionString));
+
+        // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        // app.MapCarter();
+
+        return app;
+    }
+}
